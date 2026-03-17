02da011493039d90b08a8905a3c9d0b86cc6ad90
sconezipsa <sconezipsa@gmail.com>
2026-03-17 10:44:24 +0900

    [프로그래머스] 고급 - 그룹별 조건에 맞는 식당 목록 출력하기 (#70)

---
+++ programmers/4_고급/70_그룹별_조건에_맞는_식당_목록_출력하기.sql
@@ -0,0 +1,18 @@
+-- 그룹별 조건에 맞는 식당 목록 출력하기
+-- 프로그래머스 고급 (⭐⭐⭐⭐)
+-- 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/131124
+-- 작성자: 정서영
+-- 작성일: 2026. 03. 17. 10:44:24
+
+-- 코드를 입력하세요
+SELECT M.MEMBER_NAME, R.REVIEW_TEXT, DATE_FORMAT(R.REVIEW_DATE, '%Y-%m-%d')
+FROM MEMBER_PROFILE M
+JOIN REST_REVIEW R
+ON M.MEMBER_ID=R.MEMBER_ID
+WHERE R.MEMBER_ID =(
+SELECT MEMBER_ID
+FROM REST_REVIEW
+GROUP BY MEMBER_ID
+ORDER BY COUNT(*) DESC
+LIMIT 1)
+ORDER BY R.REVIEW_DATE, R.REVIEW_TEXT
